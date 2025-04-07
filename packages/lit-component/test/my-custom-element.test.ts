8ffe658f5cdcef20f0f13df4ac7f52b8b47f4e59
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2025-04-07 18:49:46 +0200

    update web-component test to reflect changes
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- packages/lit-component/test/my-custom-element.test.ts
+++ packages/lit-component/test/my-custom-element.test.ts
@@ -4,28 +4,28 @@ import { MyCustomElement } from '../src/MyCustomElement.js';
 import '../src/my-custom-element.js';
 
 describe('MyCustomElement', () => {
-  it('has a default header "Hey there" and counter 5', async () => {
-    const el = await fixture<MyCustomElement>(html`<lit-element></lit-element>`);
+  it('has an empty default header "" and counter set to 5', async () => {
+    const el = await fixture<MyCustomElement>(html`<my-custom-element></my-custom-element>`);
 
-    expect(el.header).to.equal('Hey there');
+    expect(el.header).to.equal('');
     expect(el.counter).to.equal(5);
   });
 
   it('increases the counter on button click', async () => {
-    const el = await fixture<MyCustomElement>(html`<lit-element></lit-element>`);
+    const el = await fixture<MyCustomElement>(html`<my-custom-element></my-custom-element>`);
     el.shadowRoot!.querySelector('button')!.click();
 
     expect(el.counter).to.equal(6);
   });
 
   it('can override the header via attribute', async () => {
-    const el = await fixture<MyCustomElement>(html`<lit-element header="attribute header"></lit-element>`);
+    const el = await fixture<MyCustomElement>(html`<my-custom-element header="attribute header"></my-custom-element>`);
 
     expect(el.header).to.equal('attribute header');
   });
 
   it('passes the a11y audit', async () => {
-    const el = await fixture<MyCustomElement>(html`<lit-element></lit-element>`);
+    const el = await fixture<MyCustomElement>(html`<my-custom-element></my-custom-element>`);
 
     await expect(el).shadowDom.to.be.accessible();
   });
