0d42ba0d6062b8e392464b256fb8cf0118cd857a
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2025-04-07 17:54:33 +0200

    configure all packages with eslint flat-config
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- packages/lit-component/eslint.config.js
+++ packages/lit-component/eslint.config.js
@@ -1,50 +1,17 @@
 import js from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { readFileSync } from 'fs';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    },
-  },
-);
+const gitignore = readFileSync('.gitignore', 'utf-8');
 
-// "eslintConfig": {
-//     "parser": "@typescript-eslint/parser",
-//     "extends": [
-//       "@open-wc",
-//       "prettier"
-//     ],
-//     "plugins": [
-//       "@typescript-eslint"
-//     ],
-//     "rules": {
-//       "no-unused-vars": "off",
-//       "@typescript-eslint/no-unused-vars": [
-//         "error"
-//       ],
-//       "import/no-unresolved": "off",
-//       "import/extensions": [
-//         "error",
-//         "always",
-//         {
-//           "ignorePackages": true
-//         }
-//       ]
-//     }
-//   }
+export default defineConfig([
+  globalIgnores(gitignore.split('\n').filter(n => n && !n.startsWith('#'))),
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+]);
